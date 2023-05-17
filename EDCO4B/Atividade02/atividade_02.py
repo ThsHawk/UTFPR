21507e89ce4002d078129130fb893440447c3b47
root <root@testmachine.thshawk.fun>
2023-05-17 10:39:28 -0300

    feat(main):New methods\nMethods added: getRegN, genKeyList, readRrn, openFile. Not working properly

--- EDCO4B/Atividade02/atividade_02.py
+++ EDCO4B/Atividade02/atividade_02.py
@@ -1,23 +1,53 @@
 #Autor(es): Thales Alves, Rebecca Paulino
 #Data: 15/05/2023
 
+LINE_OFFSET = 170
+LINE_LENTH = 168
 import sys
 
-def tmpFile(lines):
-    print(lines)
+def getRegN(fileName):
+    regN = readRrn(fileName, 0)
+    regN = regN.split(" ")
+    regN = regN[0].split("=")
+    regN = regN[1]
+    return regN
+
+def genKeyList(fileName):
+    regN = getRegN(fileName)
+    for i in range(1, regN):
+        line = readRrn(filename, i)
+        line = line.split("|")
+        line[0] = line[0].split(" ")
+        print(line[0])
+        str = ""
+        for j in range(len(line[0])):
+            str = str + line[0][j]
+        line = str.upper() + line[4]
+
+
+def readRrn(fileName, rrn):
+    file = openFile(fileName)
+    file.seek(rrn*LINE_OFFSET, 0)
+    line = file.readline(LINE_LENTH)
+    file.close()
+    return line
+
+def openFile(fileName):
+    try:
+        file = open(fileName,"r")
+    except FileNotFoundError:
+        print("file: \"" + fileName + "\" not found")
+        exit()
+    else:
+        return file
 
 def main():
-    try:
-        file = open(sys.argv[1],"r")
-    except FileNotFoundError:
-        print("file: \"" + sys.argv[1] + "\" not found")
-    else:
-        lines = file.readlines()
-        file.close()
-        tmpFile(lines)
+    genKeyList(sys.argv[1])
+
 
 if len(sys.argv) == 5:
     main()
 else:
     print("invalid argument")
+    exit()
 
