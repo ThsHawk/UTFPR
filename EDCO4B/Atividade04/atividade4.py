13dc7e2db1fe2d2040795c65338706d4ee0e05d6
Thales Alves <thalesalves@alunos.utfpr.edu.br>
2023-06-19 22:44:49 -0300

    gen secondary index list working

--- EDCO4B/Atividade04/atividade4.py
+++ EDCO4B/Atividade04/atividade4.py
@@ -6,17 +6,19 @@ LINE_LENTH = 91
 LINE_OFFSET = LINE_LENTH + 2
 import sys
 
+SECND_INDEX = ["ano", "duracao", "titulo", "artista", "genero", "idioma"]
+
 def openFile(fileName, op):
     if op == "r" or op == "r+":
         try:
-            file = open(fileName, op, encoding='utf-8')
+            file = open(fileName, op)
         except FileNotFoundError:
             print("file: \"" + fileName + "\" not found")
             exit()
         else:
             return file
     else:
-        return open(fileName, op, encoding='utf-8')
+        return open(fileName, op)
     
 def addEmptyCharLine(line):
     for i in range(len(line), LINE_LENTH):
@@ -24,7 +26,7 @@ def addEmptyCharLine(line):
     return line + "\n"
 
 def addEmptyCharFile(fileName):
-    file = open(fileName, "r+")
+    file = openFile(fileName, "r+")
     lines = file.readlines()
     for i in range(len(lines)):
         lines[i] = lines[i].strip("\n")
@@ -35,27 +37,20 @@ def addEmptyCharFile(fileName):
     file.close()
 
 def readRrn(fileName, rrn):
-    file = open(fileName, "r")
+    file = openFile(fileName, "r")
     file.seek(rrn*(LINE_OFFSET), 0)
     line = file.read(LINE_LENTH)
     file.close()
     return line
 
-def storageCompaction(fileName1, fileName2, array):
-    file2 = openFile(fileName2, "w")
-    file1 = openFile(fileName1, "r")
-    file2.write(file1.readline(LINE_LENTH) + "\n")
-    file1.close()
-    for i in range(len(array)):
-        file2.write(readRrn(fileName1, array[i][1]+1) + "\n")
-    file2.close()
-
 def genKey(line, tkn):
-    print(line.encode())
     line = line.split(tkn)
     key = line[2] + line[3]
-    key = key.strip()
-    key = key.upper()
+    key = key.split(" ")
+    keyLine = ""
+    for i in range(len(key)):
+        keyLine = keyLine + key[i]
+    key = keyLine.upper()
     return key
 
 def genKeyList(fileName):
@@ -67,6 +62,15 @@ def genKeyList(fileName):
         tup = (genKey(readRrn(fileName, i), '|'), i-1)
         keyList.append(tup)
     return keyList
+
+def genSecKeyList(fileName, array, index):
+    secKeyList = []
+    for i in range(len(array)):
+        line = readRrn(fileName, array[i][1] + 1)
+        line = line.split("|")
+        tup = (line[index], array[i][0])
+        secKeyList.append(tup)
+    return secKeyList
 
 def readHeader(fileName):
     file = openFile(fileName, "r")
@@ -84,21 +88,41 @@ def readHeader(fileName):
     del aux[:]
     return headerData
 
+def searchSecndKey(Array, secKey):
+    listIdx = []
+    for i in range(len(Array)):
+        if Array[i][0] == secKey:
+            listIdx.append(i)
+    return listIdx
+
+def searchSecndKey(Array, secKey):
+    listIdx = []
+    for i in range(len(Array)):
+        if Array[i][0] == secKey:
+            listIdx.append(i)
+    return listIdx
+
 def sortAux(a):
     return a[0]
 
 def main():
-    keyList = genKeyList(sys.argv[1])
-    #keyList.sort(key=sortAux)
-    #storageCompaction(sys.argv[1], sys.argv[2], keyList)
-    del keyList[:]
+    primaryKeyList = genKeyList(sys.argv[1])
+    primaryKeyList.sort(key=sortAux)
+    secKey = "ano"
+    secondKeyList = genSecKeyList(sys.argv[1], primaryKeyList, SECND_INDEX.index(secKey))
+    listaFF = searchSecndKey(secondKeyList, "1995")
+    file = openFile(sys.argv[2], "w")
+    for i in range(len(listaFF)):
+        
 
-if len(sys.argv) == 5:
-    if sys.argv[2] == "--": #facilitar minha vida
-        addEmptyCharFile(sys.argv[1]) #usado somente para formatar os arquivos de entrada (Registros de tamanho fixo)
-        exit()
-    else:
-        main()
+if sys.argv[2] == "--": #usado somente para formatar os arquivos de entrada (Registros de tamanho fixo)
+    addEmptyCharFile(sys.argv[1]) 
+    exit()    
+elif len(sys.argv) == 4 and sys.argv[3] == "key": #gera o arquivo de indice primario
+    file = openFile(sys.argv)
+
+elif len(sys.argv) == 5:
+    main()
 else:
     print("invalid argument")
     exit()
