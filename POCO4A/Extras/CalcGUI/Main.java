96560aca9c1984cff9a04d577efebf97f1865bad
Thales Alves <thalesadsilva@outlook.com>
2023-08-23 14:39:54 -0300

    teste Swing

---
+++ POCO4A/Extras/CalcGUI/Main.java
@@ -0,0 +1,34 @@
+package POCO4A.Extras.CalcGUI;
+
+import javax.swing.*;        
+
+public class Main {
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("CalcGUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add the button w/ "Hello" label.
+        JButton testButton = new JButton("Hello");
+        frame.getContentPane().add(testButton);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
