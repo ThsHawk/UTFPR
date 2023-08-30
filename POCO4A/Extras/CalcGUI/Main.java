85116d88d14b00db24eec56f083532856526e43a
THALES ALVES DA SILVA <a2149966@vmpdc.utfpr>
2023-08-30 11:12:43 -0300

    teste durante aula do facas

--- POCO4A/Extras/CalcGUI/Main.java
+++ POCO4A/Extras/CalcGUI/Main.java
@@ -1,51 +1,70 @@
 package POCO4A.Extras.CalcGUI;
 
-import javax.swing.*;
-
-import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.event.*;
-import POCO4A.Extras.CalcGUI.graphic.*;       
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+ 
 public class Main {
+    public static void addComponentsToPane(Container pane) {
+        pane.setLayout(null);
+ 
+        //addATextDisplay("teste", pane);
+        addANumPad(pane);
+
+    }
+    
+    private static void addANumPad(Container container){
+        JPanel numPad = new JPanel(new GridLayout(4, 4));
+        addAButton("Button 1", numPad);
+        addAButton("Button 2", numPad);
+        addAButton("Button 3", numPad);
+        addAButton("Long-Named Button 4", numPad);
+        addAButton("5", numPad);
+        container.add(numPad);
+        
+    }
+    
+    private static void addAButton(String text, Container container) {
+        JButton button = new JButton(text);
+        button.setSize(50, 50);
+        //button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        container.add(button);
+    }
+
+    private static void addATextDisplay(String text, Container container){
+        JTextField tf = new JTextField(text, 20);
+        tf.setSize(250, 100);
+        tf.setAlignmentX(Component.CENTER_ALIGNMENT);
+        container.add(tf);
+    }
+ 
     /**
      * Create the GUI and show it.  For thread safety,
      * this method should be invoked from the
      * event-dispatching thread.
      */
-
-    private static String var;
-
     private static void createAndShowGUI() {
         //Create and set up the window.
         JFrame frame = new JFrame("CalcGUI");
-        frame.setLayout(new GridLayout(2, 1));
         frame.setSize(400, 500);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //Separador
-        JPanel display = new JPanel();
-        display.setLayout(new BorderLayout(100, 100));
-        display.setBackground(Color.RED);
-        JTextField tf = new JTextField(var);
-        tf.setSize(250, 70);
-        display.add(tf, BorderLayout.CENTER);
-        //teste label
-        frame.add(display, BorderLayout.NORTH);
-        
-        
-        //Add the button w/ "Hello" label.
-
-
-        //frame.getContentPane().add(testButton1);
-        //frame.getContentPane().add(tf, BorderLayout.LINE_START);
-
+ 
+        //Set up the content pane.
+        addComponentsToPane(frame.getContentPane());
+ 
         //Display the window.
+        //frame.pack();
         frame.setVisible(true);
     }
-
+ 
     public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
