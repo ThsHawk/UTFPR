bfe7a29f2dd6684f461cc05a1da5c926e88b6be8
Thales Alves <thalesadsilva@outlook.com>
2023-10-25 20:00:31 -0300

    trabalho SO - quase pronto - falta TME

--- SOCO5A/Trabalho01/main.py
+++ SOCO5A/Trabalho01/main.py
@@ -1,5 +1,19 @@
 import sys
 
+def sortTime(a):
+    return a[1]
+
+def sortArrive(a):
+    return a[2]
+
+def arrived(counter, queue):
+    if queue == None:
+        return 0
+    elif queue[2] <= counter:
+        return 1
+    else:
+        return 0
+
 def main():
     argvLen = len(sys.argv)
     if argvLen < 2:
@@ -23,8 +37,39 @@ def main():
             lines[i] = lines[i][:len(lines[i])-1]
             lines[i] = lines[i].split("\t")
             lines[i].pop(0)
-        print(lines)
-            
+
+        queue = list()
+        for i in range(len(lines[0])):
+            queue.append((lines[0][i], int(lines[1][i]), int(lines[2][i])))
+        queue.sort(key=sortArrive)
+        
+        counter = 0
+        execTime = 0
+        waitQueue = list()
+        waitQueue.append(queue[0])
+        queue.pop(0)
+        records = list()
+        while(len(waitQueue) != 0):
+            if len(queue) != 0:
+                if arrived(counter, queue[0]):
+                    waitQueue.append(queue[0])
+                    waitQueue.sort(key=sortTime)
+                    queue.pop(0)
+            aux = list(waitQueue[0])
+            aux[1] -= 1
+            waitQueue[0] = (aux[0], aux[1], aux[2])
+            counter += 1
+            execTime += 1
+            print(aux)
+            if waitQueue[0][1] == 0:
+                records.append((waitQueue[0][0], waitQueue[0][2], counter, execTime))
+                execTime = 0
+                waitQueue.pop(0)
+
+        print(records)
+                
+
+
         file.close()
 
 main()
