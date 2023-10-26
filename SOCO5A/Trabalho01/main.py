8bebc0d3919b828c59683486284513ca333095a7
Thales Alves <thalesadsilva@outlook.com>
2023-10-25 21:37:56 -0300

    Trabalho de SO - Final

--- SOCO5A/Trabalho01/main.py
+++ SOCO5A/Trabalho01/main.py
@@ -9,14 +9,6 @@ def sortTime(a):
 def sortArrive(a):
     return a[2]
 
-def arrived(counter, queue):
-    if queue == None:
-        return 0
-    elif queue[2] <= counter:
-        return 1
-    else:
-        return 0
-
 def main():
     argvLen = len(sys.argv)
     if argvLen < 2:
@@ -40,51 +32,55 @@ def main():
             lines[i] = lines[i][:len(lines[i])-1]
             lines[i] = lines[i].split("\t")
             lines[i].pop(0)
-
+        #geração da fila por ordem de chegada
         queue = list()
         for i in range(len(lines[0])):
             queue.append((lines[0][i], int(lines[1][i]), int(lines[2][i])))
         queue.sort(key=sortArrive)
         
-        counter = 0
+        timeUnit = 0
         processAmount = len(queue)
-        waitingTime = queue.copy()
+        queueCopy = queue.copy()
         waitQueue = list()
         waitQueue.append(queue[0])
         queue.pop(0)
         records = list()
+
+        #simulação de execução dos processos // timeline
         print("\n\n---TimeLine---")
         while(len(waitQueue) != 0):
             if len(queue) != 0:
-                if arrived(counter, queue[0]):
+                if queue[0][2] <= timeUnit:
                     waitQueue.append(queue[0])
                     waitQueue.sort(key=sortTime)
                     queue.pop(0)
             aux = list(waitQueue[0])
             aux[1] -= 1
             waitQueue[0] = (aux[0], aux[1], aux[2])
-            counter += 1
+            timeUnit += 1
             print(aux[0].upper() + " ", end="")
             if waitQueue[0][1] == 0:
-                records.append((waitQueue[0][0], counter - waitQueue[0][2], waitQueue[0][2]))
+                records.append((waitQueue[0][0], timeUnit - waitQueue[0][2], waitQueue[0][2]))
                 waitQueue.pop(0)            
-        print("\n\n")
+        print("\n")
+
+        #calculo do tempo medio de resposta
         media = 0
         for i in range(processAmount):
             media = media + records[i][1]
         media = media / processAmount
         print("---TempoMedioDeResposta---")
         print("TMR - " + str(media) + "\n")
-        waitingTime.sort(key=sortName)
-        records.sort(key=sortName)
-        time = 0
-        for i in range(processAmount):
-            time = time + (records[i][1] - waitingTime[i][1])
-        time = time / processAmount
-        print("---TempoMedioDeEspera---")
-        print("TME - " + str(time) + "\n\n")
-        
 
+        #calculo do tempo medio de espera
+        queueCopy.sort(key=sortName)
+        records.sort(key=sortName)
+        waitingTime = 0
+        for i in range(processAmount):
+            waitingTime = waitingTime + (records[i][1] - queueCopy[i][1])
+        waitingTime = waitingTime / processAmount
+        print("---TempoMedioDeEspera---")
+        print("TME - " + str(waitingTime) + "\n\n")    
 
         file.close()
 
